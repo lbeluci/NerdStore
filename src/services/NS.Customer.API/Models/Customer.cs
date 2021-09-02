70ceb64976b9378cba584c006cd535c9f989fa38
Luiz Henrique Beluci Terra <lbeluci@gmail.com>
2021-09-02 08:05:40 -0300

    feat: Adding the Customer entity.

---
+++ src/services/NS.Customer.API/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using NS.Core.DomainObjects;
+using System;
+
+namespace NS.Customer.API.Models
+{
+    public class Customer : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Cpf { get; private set; }
+
+        public bool Deleted { get; private set; }
+
+        public Address Address { get; private set; }
+
+        /*Remeber: This constructor is exclusive for EF*/
+        protected Customer()
+        {
+        }
+
+        public Customer(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            Deleted = false;
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+        }
+
+        public void SetAddress(Address address)
+        {
+            Address = address;
+        }
+    }
+}

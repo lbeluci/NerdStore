90ca3aa33c805a3e98e24622661e14a72b89ea64
Luiz Henrique Beluci Terra <lbeluci@gmail.com>
2021-09-03 08:26:49 -0300

    feat: Refactoring project names.

---
+++ src/services/NS.Customers.API/Data/CustomersContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NS.Core.Data;
+using NS.Customers.API.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NS.Customers.API.Data
+{
+    public class CustomersContext : DbContext, IUnitOfWork
+    {
+        public CustomersContext(DbContextOptions<CustomersContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            foreach (var foreignKeys in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKeys.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomersContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/services/NS.Customers.API/Data/Mappings/CustomerMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NS.Core.DomainObjects;
+using NS.Customers.API.Models;
+
+namespace NS.Customers.API.Data.Mappings
+{
+    public class CustomerMapping : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name).IsRequired().HasColumnType("varchar(200)");
+
+            builder.OwnsOne(c => c.Cpf, ot => 
+            {
+                ot.Property(c => c.Number).IsRequired().HasMaxLength(Cpf.MaxLength).HasColumnName("Cpf").HasColumnType($"varchar({Cpf.MaxLength})");
+            });
+
+            builder.OwnsOne(c => c.Email, ot =>
+            {
+                ot.Property(c => c.Address).IsRequired().HasColumnName("Email").HasColumnType($"varchar({Email.MaxLength})");
+            });
+
+
+        }
+    }
+}
